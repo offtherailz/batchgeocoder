46dbe7fc554711156bda4942af2f0bcc34ca20d7
Damiano <damianofds@gmail.com>
2012-06-06 10:16:06 +0200

    #2

--- src/main/java/it/geosolutions/geobatchcoder/search/SearchEngine.java
+++ src/main/java/it/geosolutions/geobatchcoder/search/SearchEngine.java
@@ -1,15 +1,20 @@
 package it.geosolutions.geobatchcoder.search;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Logger;
-
 import it.geosolutions.geobatchcoder.model.Location;
 import it.geosolutions.geobatchcoder.persistance.CSVRepositoryReader;
 import it.geosolutions.geobatchcoder.persistance.CSVRepositoryWriter;
 import it.geosolutions.geobatchcoder.persistance.Input;
 import it.geosolutions.geobatchcoder.persistance.Output;
 import it.geosolutions.geobatchcoder.persistance.OutputFileType;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
 
 public class SearchEngine {
 
@@ -20,15 +25,23 @@ public class SearchEngine {
 	private Search searcher;
 	private Output listGeocoded;
 	private Output outDiscarded;
+	private Configuration conf;
 	
 	public SearchEngine(){
+		try {
+			conf = new PropertiesConfiguration("configuration.properties");
+		} catch (ConfigurationException e) {
+			LOG.log(Level.SEVERE, "failed to load configurations");
+		}
 		repo = new CSVRepositoryReader();
-		searcher = new NominatimSearch();
+		searcher = new NominatimSearch(conf.getString("email"));
 		listGeocoded = new CSVRepositoryWriter(OutputFileType.GEOCODED);
 		outDiscarded = new CSVRepositoryWriter(OutputFileType.DISCARDED);
+		
 	}
 	
 	public void runSearch(){
+		String email = conf.getString("email");
 		List<Location> geocodedList = new ArrayList<Location>();
 		repo.loadLocations();
 		boolean outcome = false;
@@ -42,6 +55,12 @@ public class SearchEngine {
 			else{
 				geocodedList.add(el);
 				LOG.info("Geocoded " + el.getName());
+			}
+			
+			try {
+				Thread.sleep(1300);
+			} catch (InterruptedException e) {
+				new RuntimeException(e);
 			}
 		}
 		listGeocoded.storeLocations(geocodedList);
